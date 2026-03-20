2f01fa4bc65b2d241dcaecac08f6d24ca2483ba9
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2026-03-20 15:26:55 +0000

    update globals styles

--- my-portfolio/src/theme/GlobalStyle.ts
+++ my-portfolio/src/theme/GlobalStyle.ts
@@ -9,6 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
   html {
     scroll-behavior: smooth;
+    font-size: 16px;
   }
 
   body {
@@ -16,6 +17,8 @@ export const GlobalStyle = createGlobalStyle`
     color: ${({ theme }) => theme.colors.text};
     font-family: Inter, system-ui, sans-serif;
     -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    line-height: 1.5;
   }
 
   a {
