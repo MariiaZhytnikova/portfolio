89e249295dc5fec70eaa893304fb03c8f62e107c
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2026-03-20 14:24:11 +0000

    fix base path in vite config

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/my-portfolio/",
+  base: "/portfolio/",
   plugins: [react(), svgr()],
 })
