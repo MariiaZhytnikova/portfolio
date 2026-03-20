fab4b03a1aed4f477c15c89771d71610b2f75133
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2026-03-20 13:07:27 +0000

    fix vite config

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/portfolio/my-portfolio/",
   plugins: [react(), svgr()],
 })
