7e672df8a1b8333c852dd0ee8f132ba26de63662
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2026-05-12 13:36:57 +0000

    update(expirience): update expirience information

--- my-portfolio/src/components/data/ExperienceData.tsx
+++ my-portfolio/src/components/data/ExperienceData.tsx
@@ -2,37 +2,23 @@
 
 export  const experiences = [
   {
-    content:  "In November 2024, I joined Hive Helsinki, a fully project-based program." +
-              "There, I’ve been diving into C and C++ projects, learning low-level system" +
-              " programming, and collaborating with talented peers to create innovative " +
+    content:  "In November 2024, I joined Hive Helsinki, a fully project-based program. " +
+              "There, I’ve been diving into C and C++ projects, learning low-level system " +
+              "programming, and collaborating with talented peers to create innovative " +
               "solutions. It’s been an exciting journey so far, and I’m eager to see where it takes me!",
   },
   {
-    content:  "Before moving into tech, I had the opportunity to work as a scientific consultant at NIBR " +
-              "for several years. In this role, I collaborated with researchers to design and " +
-              "implement data analysis pipelines, utilizing Python and R to extract insights from " +
-              "complex datasets. This experience honed my problem-solving skills and deepened my " +
+    content:  "Before moving into tech, I had the opportunity to work as a scientific consultant at NIBR and " +
+              "as a Research Officer at Institute of Radiophysics and Electronics NASU. This experience honed " +
+              "my problem-solving skills and deepened my " +
               "appreciation for the power of programming in scientific research.",
 
   },
   {
-    content:  "I worked as a Research Officer at IRE NASU in Kharkiv, Ukraine, from December 2017 to " +
-              "June 2024. During this time, I conducted molecular dynamics simulations and structural" +
-              "analyses of protein–DNA interactions, managing complex datasets and computational" +
-              "workflows. This experience strengthened my problem-solving abilities, attention to detail," +
-              "and capacity to handle technically challenging tasks—skills that are highly relevant in" +
-              "software development and analytical roles.",
-  },
-  {
-    content:  "When I transitioned into tech, I realized many of my previous skills were directly" +
-              " transferable. My experience with data analysis, problem-solving, and working with complex" +
-              " systems has given me a strong foundation for learning programming languages and software" +
-              " development practices. I’m excited to apply these skills in new ways and continue growing" +
-              " as a developer.",
-  },
-  {
-    content:  "My curiosity and passion for learning have driven me to explore various programming languages " +
-              "and technologies, and I’m always excited to take on new challenges and expand my skill set." +
-              "I’m focused on growing as a developer and building projects that actually do something useful (or fun).",
+    content:  "When I transitioned into tech, I realized many of my previous skills were directly " +
+              "transferable. My experience with data analysis, problem-solving, and working with complex " +
+              "systems has given me a strong foundation for learning programming languages and software " +
+              "development practices. I’m excited to apply these skills in new ways and continue growing " +
+              "as a developer.",
   },
 ];
